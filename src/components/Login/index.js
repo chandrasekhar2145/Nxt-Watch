1074d3c57f11b9f423a810ca0e21ca8d5eff8da1
Chandrasekhar Kuruva <148301206+chandrasekhar2145@users.noreply.github.com>
2024-09-16 14:26:52 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -39,7 +39,7 @@ class Login extends Component {
 
   onSubmitSuccess = jwtToken => {
     const {history} = this.props
-    Cookies.set('jwt_token', jwtToken, {expires: 100, path: '/'})
+    Cookies.set('jwt_token', jwtToken, {expires: 30, path: '/'})
     history.replace('/')
   }
 
